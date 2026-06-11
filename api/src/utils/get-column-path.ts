39b66091365c0f6b9162215c295e83f30603c372
cairncms-dev <hello@cairncms.dev>
2026-06-11 14:25:07 -0500

    Return validation errors for unknown filter and sort fields
    Signed-off-by: cairncms-dev <hello@cairncms.dev>

--- api/src/utils/get-column-path.ts
+++ api/src/utils/get-column-path.ts
@@ -71,7 +71,7 @@ export function getColumnPath({ path, collection, aliasMap, relations, schema }:
 			// Nested level alias field
 			else if (
 				remainingParts.length === 1 &&
-				schema.collections[parent]!.fields[remainingParts[0]!]!.type === 'alias'
+				schema.collections[parent]?.fields[remainingParts[0]!]?.type === 'alias'
 			) {
 				remainingParts.push(schema.collections[relation!.related_collection!]!.primary);
 				addNestedPkField = schema.collections[relation!.related_collection!]!.primary;
