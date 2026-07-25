16e8d0a0231a96b4ed1865fa1ad2cb7037453145
cairncms-dev <hello@cairncms.dev>
2026-07-25 06:48:55 -0500

    Cover confined item writes with blackbox tests
    Signed-off-by: cairncms-dev <hello@cairncms.dev>

--- tests/blackbox/extensions/cairncms-extension-confined-items-system/api.js
+++ tests/blackbox/extensions/cairncms-extension-confined-items-system/api.js
@@ -1,10 +1,26 @@
 var CairnOperation = (() => {
 	const handler = async ({ options }, { host }) => {
-		if (options.action === 'readOne') {
-			return host.items.readOne(options.collection, options.key, options.query ?? {});
+		switch (options.action) {
+			case 'readOne':
+				return host.items.readOne(options.collection, options.key, options.query ?? {});
+			case 'createOne':
+				return host.items.createOne(options.collection, options.payload);
+			case 'createMany':
+				return host.items.createMany(options.collection, options.payloads);
+			case 'updateOne':
+				return host.items.updateOne(options.collection, options.key, options.payload);
+			case 'updateMany':
+				return host.items.updateMany(options.collection, options.keys, options.payload);
+			case 'deleteOne':
+				return host.items.deleteOne(options.collection, options.key);
+			case 'deleteMany':
+				return host.items.deleteMany(options.collection, options.keys);
+			case 'read':
+			case 'readMany':
+				return host.items.readMany(options.collection, options.query ?? {});
+			default:
+				throw new Error(`Unsupported items probe action: ${options.action}`);
 		}
-
-		return host.items.readMany(options.collection, options.query ?? {});
 	};
 
 	return { default: { id: 'cairncms-extension-confined-items-system', handler } };
